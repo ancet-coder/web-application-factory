eb528e29a46c0fa31d882059c0a33fc208464b34
Ankit kumar <140902551+ankit-kumar-2024@users.noreply.github.com>
2024-03-12 08:06:39 +0530

    changes in 1st update

--- educatster/src/components/carousel.js
+++ educatster/src/components/carousel.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
+// import image2 from './images/image2.jpg';
+// import image3 from './images/image3.jpg';
 
 
 const Carousel = () => {
